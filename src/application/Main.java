d70e1bc606fbc0ca41007a3f07e86f747390fcc3
Yazan Abbas <yazanabbas@Yazans-MacBook-Pro.local>
2021-03-02 23:07:58 +0100

    Task class created

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,16 @@
 package application;
+import java.time.LocalDate;
+import java.util.Date;
+
+import static application.Task.createTask;
 
 public class Main {
 
 public static void main (String [] args){
 
-    System.out.println("To do list");
+    LocalDate date = LocalDate.of(2020, 12, 12);
+
+    System.out.println(createTask("1", "Coffee", date, "active", "coffe project"));
 
 }
 
