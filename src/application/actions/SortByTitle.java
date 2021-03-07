6665179f934b4afaa2a82c9f15f447bc0fecdfb4
Yazan <yazann.abbas@gmail.com>
2021-03-07 19:44:05 +0100

    add SortBYTitle

---
+++ src/application/actions/SortByTitle.java
@@ -0,0 +1,29 @@
+package application.actions;
+
+import application.app.Menu;
+import application.app.Task;
+import application.app.ToDoList;
+
+import java.util.Comparator;
+
+public class SortByTitle {
+
+    public static void sortByTitle(){
+        String displayFormat = "%-20s %-35s %-10s %-10s";
+
+        if (ToDoList.tasks.size()>0) {
+            System.out.println(String.format(displayFormat,"DUE DATE","TITLE","PROJECT" , "Done"));
+            System.out.println(String.format(displayFormat,"########","#####","#######" , "####"));
+        } else {
+            System.out.println(Menu.CYAN + "No tasks " + Menu.RESET);
+        }
+
+        ToDoList.tasks.stream()
+                .sorted(Comparator.comparing(Task::getProject))
+                .forEach(task -> System.out.println(String.format(displayFormat,task.getProject(),
+                        task.getTitle(),
+                        task.getDueDate(),
+                        (task.isDone()?"YES":"NO")
+                )));
+    }
+}
