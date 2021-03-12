02cfc1407c2ada27be00f8a54c34a7de239ecde8
Yazan <yazann.abbas@gmail.com>
2021-03-12 16:38:52 +0100

    Documintation for the Display Class

--- src/Main/application/actions/Display.java
+++ src/Main/application/actions/Display.java
@@ -1,9 +1,14 @@
 package Main.application.actions;
+
 import Main.application.app.Menu;
 import Main.application.app.ToDoList;
 
 
 public class Display {
+
+    /* this method is created to display the contents of todoList
+       sortBy a string holding a number, "2" for sorting by project,
+       otherwise it will sort by by date */
     public static void displayTasks(String sortBy) {
 
         System.out.println("########################################################################");
@@ -14,26 +19,31 @@ public class Display {
                         " )");
 
         System.out.println("########################################################################");
-        if (sortBy.equals("2")) { SortByTitle.sortByTitle();}
-        else { SortByDate.sortByDate();}
+        if (sortBy.equals("2")) {
+            SortByTitle.sortByTitle();
+        } else {
+            SortByDate.sortByDate();
+        }
     }
+
+    /*this method was designed to display the contents of todoList with first column as task number*/
 
     public static void indexTasks() {
         String displayFormat = "%-4s%-35s %-20s %-10s %-10s";
 
-        if (ToDoList.tasks.size()>0) {
-            System.out.println(String.format(displayFormat,"Index","TITLE","PROJECT","DUE DATE","Done"));
-            System.out.println(String.format(displayFormat,"#####","#####","#######","#########","####"));
+        if (ToDoList.tasks.size() > 0) {
+            System.out.println(String.format(displayFormat, "Index", "TITLE", "PROJECT", "DUE DATE", "Done"));
+            System.out.println(String.format(displayFormat, "#####", "#####", "#######", "#########", "####"));
         } else {
             System.out.println(Menu.CYAN + "No tasks to show" + Menu.RESET);
         }
         ToDoList.tasks.stream()
                 .forEach(task -> System.out.println(String.format(displayFormat,
-                        ToDoList.tasks.indexOf(task)+1,
+                        ToDoList.tasks.indexOf(task) + 1,
                         task.getTitle(),
                         task.getProject(),
                         task.getDueDate(),
-                        (task.isDone()?"YES":"NO")
+                        (task.isDone() ? "YES" : "NO")
                 )));
     }
 }
