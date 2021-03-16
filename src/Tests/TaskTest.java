b2d37a5cefc8f3a06c556d53b25cfd988e283066
Yazan <yazann.abbas@gmail.com>
2021-03-16 22:47:02 +0100

    First test fixed

--- src/Tests/TaskTest.java
+++ src/Tests/TaskTest.java
@@ -3,10 +3,32 @@ package Tests;
 import Main.application.controllers.Task;
 import org.junit.jupiter.api.Test;
 
+
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
+    Task task;
+
+
+
+    @Test
+    public void testSettingEmptyTitle() {
+        boolean successful=false;
+
+        try {
+            task.setTitle("");
+            fail("should have thrown a NullPointerException");
+        } catch (NullPointerException e) {
+            successful = true;
+        }
+
+        assertEquals(true,successful);
+    }
+
+
+
 
 }
