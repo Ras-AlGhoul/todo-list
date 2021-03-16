5464f69191d280c83b56c7e25db6fe18106e633f
Yazan <yazann.abbas@gmail.com>
2021-03-16 20:18:35 +0100

    fixed format

--- src/Main/application/view/TaskCounter.java
+++ src/Main/application/view/TaskCounter.java
@@ -16,7 +16,7 @@ public class TaskCounter {
     /**
      * created this method to count the number of done tasks with Done status
      * @param tasks is list of the tasks
-     * @return number of tasks with completed status
+     * @return number of tasks with Done status
      */
 
     public static int doneCount(ArrayList<Task> tasks) {
@@ -28,7 +28,7 @@ public class TaskCounter {
     /**
      * created this method to count the number of uncompleted tasks with incompleted status
      * @param tasks is list of the tasks
-     * @return number of tasks with incomplete status
+     * @return number of tasks with incompleted status
      */
     public static int inCompletedCount(ArrayList<Task> tasks) {
         return (int) tasks.stream()
